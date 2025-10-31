ac4efdcf9d7c93cb88335dd998d9df14bd36ba24
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-10-31 19:13:33 -0300

    Novos componentes vue

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,17 @@
+<template>
+  <Button icon="pi pi-sun" @click="toggleTheme" />
+</template>
+
+<script setup lang="ts">
+import { usePrimeVue } from 'primevue/config';
+const primevue = usePrimeVue();
+
+function toggleTheme(){
+    const currentTheme = localStorage.getItem('theme') || 'lara-light-blue';
+    const nextTheme = currentTheme.includes('light') ? 'lara-dark-blue' : 'lara-light-blue';
+    import(`primevue/resources/themes/${nextTheme}/theme.css`);
+    localStorage.setItem('theme', nextTheme);
+}
+
+</script>
+
