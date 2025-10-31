89df2f2af948cb903d21e0f29fe6dc77547bb3f4
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-10-31 19:35:54 -0300

    Instalação e teste do tailwind no vuejs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const people = [
+    {
+      name: 'Calvin Hawkins',
+      email: 'calvin.hawkins@example.com',
+      image: 'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      name: 'Kristen Ramos',
+      email: 'kristen.ramos@example.com',
+      image: 'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      name: 'Ted Fox',
+      email: 'ted.fox@example.com',
+      image: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  ]
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <ul class="divide-y divide-gray-200">
+    <li v-for="person in people" :key="person.email" class="flex py-4">
+      <img class="size-10 rounded-full" :src="person.image" alt="" />
+      <div class="ml-3">
+        <p class="">{{ person.name }}</p>
+        <p class="text-sm text-gray-500">{{ person.email }}</p>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
