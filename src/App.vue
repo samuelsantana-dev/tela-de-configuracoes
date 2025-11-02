47403da20c43e5ac9bc6846d09132110831f98f6
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-11-02 16:50:39 -0300

    :sparkles: Ajustado arquivo app vue para sidBar e rotas

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue';
 </script>
 
 <template>
   <div>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+    <Sidebar />
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <router-view />
+  </div>
+</template>
